19205ee6ea32118ad3b7ff7fe9d0bf4336d8ef21
heyao <jackay-hy@qq.com>
2019-06-05 13:19:12 +0800

    init: init contrib for data reader module

---
+++ htorch/contrib/data/reader/__init__.py
@@ -0,0 +1,4 @@
+from htorch.contrib.data.reader.chn_det import CHNDetReader, CHNDetOriginalReader
+from htorch.contrib.data.reader.chn import CHNReader
+from htorch.contrib.data.reader.conll_2003 import CoNLL2003Reader
+from htorch.contrib.data.reader.few_rel import FewRelReader
